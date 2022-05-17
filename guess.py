f5f9b69e5a53ab42f8ec5b707f679c10e0171e67
Norm Chiu <chou.no98@gmail.com>
2022-05-17 11:59:26 +0800

    second version

--- guess.py
+++ guess.py
@@ -1,14 +1,17 @@
 import random 
 r = random.randint(1, 100)
-
 tr = r
+i = 0
 while True:
-	ans = input('請輸入數字：')
+	i += 1 #是i = i + 1的快速寫法
+	ans = input('請輸入數字：') #input. print存到左邊都是字串
 	ans = int(ans)
 	if ans == tr:
 		print('正確解答')
+		print('這是你第', i, '次猜測')
 		break
 	elif ans > tr:
 		print('比答案大')
 	elif ans < tr:
 		print('比答案小')
+	print('這是你第', i, '次猜測') #可以放在最底下，因為if跟兩個elif是同一個架構的
